9cba6fbe7f925b80dcbe4d193baec16cba3b6325
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-06-16 13:01:38 -0600

    Serve css natively from index file

--- server/server.js
+++ server/server.js
@@ -6,13 +6,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+//Middleware
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/styles', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.css'));
-});
+//endpoints
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
+
+// app.get('/styles', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.css'));
+// });
 
 const port = process.env.PORT || 4005; //4005 is to run server locally
 
